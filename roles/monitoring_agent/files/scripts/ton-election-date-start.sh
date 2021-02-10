54698922eeed1c8b0ee698b6bea4b74a687221b9
itgoldio <itgold@itglobal.com>
2021-02-10 12:26:56 +0300

    fix scripts and some mistakes in playbook
    - add script ton-node-participant-state.sh
    - add script ton-depool-validation-request.sh
    - add .sh for all scripts
    - fix logic of ton-election-date-start.sh
    - fix logic of ton-election-date-end.sh
    - change dasboard

--- roles/monitoring_agent/files/scripts/ton-election-date-start.sh
+++ roles/monitoring_agent/files/scripts/ton-election-date-start.sh
@@ -16,8 +16,15 @@ ELECTIONS_START=$($TON_CLI --url $TON_DAPP runget $ELECTOR_ADDR active_election_
 if (( $ELECTIONS_START == 0 ));
     then
         echo "-1";
-    else
-        echo $ELECTIONS_START;
+        exit
 fi
 
+ELECTOR_CONFIG=`$TON_CLI --url $TON_DAPP getconfig 15` 
+ELECTOR_CONFIG_JSON=$(echo $ELECTOR_CONFIG | awk '{split($0, a, "p15:"); print a[2]}')
+ELECTOR_CONFIG_VALIDATORS_ELECTED_FOR=`echo "$ELECTOR_CONFIG_JSON" | jq ".validators_elected_for"`
+ELECTOR_CONFIG_ELECTIONS_START_BEFORE=`echo "$ELECTOR_CONFIG_JSON" | jq ".elections_start_before"`
+ELECTOR_CONFIG_ELECTIONS_END_BEFORE=`echo "$ELECTOR_CONFIG_JSON" | jq ".elections_end_before"`
+ELECTOR_CONFIG_STAKE_HELD_FOR=`echo "$ELECTOR_CONFIG_JSON" | jq ".stake_held_for"`
+
+echo $(($ELECTIONS_START - $ELECTOR_CONFIG_ELECTIONS_START_BEFORE))
 
