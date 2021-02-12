ae644bd40501bbb97ee6b9e6951b970ce434bd4c
itgoldio <itgold@itglobal.com>
2021-02-12 23:59:57 +0300

    update ton-depool-ticktok.sh
    - don't repeat if sender in election

--- roles/monitoring_agent/files/scripts/ton-depool-ticktok.sh
+++ roles/monitoring_agent/files/scripts/ton-depool-ticktok.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eE
 
+
+TON_ELECTION_TICKTOK_IS_SENDED="depool.ticktock.sended"
+
 # export ton environments
 . ton-env.sh
 
@@ -9,5 +12,47 @@ ton-check-env.sh DEPOOL_ADDR
 ton-check-env.sh TIK_ADDR
 ton-check-env.sh TIK_PRV_KEY
 
-$TON_CLI --url $TON_DAPP depool --addr $DEPOOL_ADDR ticktock -w $TIK_ADDR -s $TIK_PRV_KEY
+if [ $# == 1 ];then
+   if [ $1 = '-f' ] || [ $1 = '-force' ];then
+       echo "INFO: force mod"
+       $TON_CLI --url $TON_DAPP depool --addr $DEPOOL_ADDR ticktock -w $TIK_ADDR -s $TIK_PRV_KEY
+       exit
+    fi
+fi
+
+#check election data
+ELECTION_STATE=$(ton-election-state.sh)
+if [ $ELECTION_STATE != "ACTIVE" ];then
+   echo "INFO: Election is not started"
+   exit
+fi
+
+# get elector address
+ELECTOR_ADDR="-1:$($TON_CLI --url $TON_DAPP  getconfig 1 | grep 'p1:' | sed 's/Config p1:[[:space:]]*//g' | tr -d \")"
+
+# get elector start (unixtime)
+ELECTIONS_DATE=$($TON_CLI --url $TON_DAPP runget $ELECTOR_ADDR active_election_id  | grep 'Result:' | sed 's/Result:[[:space:]]*//g' | tr -d \"[])
+
+TON_ELECTION_SUBFOLDER="$TON_ELECTION_FOLDER/$ELECTIONS_DATE"
+if [ ! -d $TON_ELECTION_SUBFOLDER ]; then
+   mkdir $TON_ELECTION_SUBFOLDER
+fi
+
+if [ -f $TON_ELECTION_SUBFOLDER/$TON_ELECTION_TICKTOK_IS_SENDED ]; then
+   echo "INFO: ticktok already sended"
+   exit
+fi
+   
+DEPOOL_TICKTOK_RESULT="$($TON_CLI --url $TON_DAPP depool --addr $DEPOOL_ADDR ticktock -w $TIK_ADDR -s $TIK_PRV_KEY)"
+
+DEPOOL_TICKTOK_TRANSACTION_ID="$(echo $DEPOOL_TICKTOK_RESULT | awk -F'Result: ' '{print $2}' | jq -r '.transId')"
+if [ -z "$DEPOOL_TICKTOK_TRANSACTION_ID" ];then
+   echo "ERROR: can't create ticktok $DEPOOL_TICKTOK_RESULT"
+   exit
+fi
+
+touch $TON_ELECTION_SUBFOLDER/$TON_ELECTION_TICKTOK_IS_SENDED
+echo $DEPOOL_TICKTOK_RESULT >> $TON_ELECTION_SUBFOLDER/$TON_ELECTION_TICKTOK_IS_SENDED
+echo "$DEPOOL_TICKTOK_RESULT"
+
 
