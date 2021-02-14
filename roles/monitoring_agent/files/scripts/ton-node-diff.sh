d288f58bbd673ec4d3b64d62ff6d423f01faf419
Gavrilov, Evgueni <evgueni.gavrilov@vStack.com>
2021-02-14 16:09:59 +0300

    add simple cycle view

--- roles/monitoring_agent/files/scripts/ton-node-diff.sh
+++ roles/monitoring_agent/files/scripts/ton-node-diff.sh
@@ -1,17 +1,33 @@
 #!/bin/bash -eE
 
+cycle_view=0
+while getopts "f" opt; do
+	case $opt in
+		f)	cycle_view=1
+		;;
+	esac
+done
+
 # export ton environments
 . ton-env.sh
 
 ton-check-env.sh TON_CONSOLE
 ton-check-env.sh TON_CONSOLE_CONFIG
 
-NODE_DIFF=$($TON_CONSOLE -C $TON_CONSOLE_CONFIG -c getstats | grep 'timediff' | sed 's/[[:space:]]*"timediff":[[:space:]]*//g' | sed 's/,//g')
+show_node_diff ()
+{
+	NODE_DIFF=$($TON_CONSOLE -C $TON_CONSOLE_CONFIG -c getstats | grep 'timediff' | sed 's/[[:space:]]*"timediff":[[:space:]]*//g' | sed 's/,//g')
+	if [ -z $NODE_DIFF ]; then 
+	   echo "-1"
+	   exit
+	fi
+	echo $NODE_DIFF
+}
 
-if [ -z $NODE_DIFF ]; then 
-   echo "-1"
-   exit
+if [ $cycle_view -eq 0 ]; then
+	show_node_diff
 fi
 
-echo $NODE_DIFF
-
+while [ $cycle_view -gt 0 ]; do
+	show_node_diff; sleep 1
+done
