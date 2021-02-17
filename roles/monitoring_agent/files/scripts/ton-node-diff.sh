206b63de244b7adad1a761b5f61f7ac749ec8dcd
itgoldio <itgold@itglobal.com>
2021-02-17 12:45:20 +0300

    fix ton-node-diff.sh for monitoring

--- roles/monitoring_agent/files/scripts/ton-node-diff.sh
+++ roles/monitoring_agent/files/scripts/ton-node-diff.sh
@@ -2,10 +2,10 @@
 
 cycle_view=0
 while getopts "f" opt; do
-	case $opt in
-		f)	cycle_view=1; cycle_interval=60
-		;;
-	esac
+        case $opt in
+                f)      cycle_view=1; cycle_interval=60
+                ;;
+        esac
 done
 shift $(($OPTIND - 1))
 
@@ -17,19 +17,21 @@ ton-check-env.sh TON_CONSOLE_CONFIG
 
 show_node_diff ()
 {
-	TS=$(date)
-	NODE_DIFF=$($TON_CONSOLE -C $TON_CONSOLE_CONFIG -c getstats | grep 'timediff' | sed 's/[[:space:]]*"timediff":[[:space:]]*//g' | sed 's/,//g')
-	if [ -z $NODE_DIFF ]; then 
-	   echo "-1"
-	   exit
-	fi
-	echo "${TON_DAPP} ${TS}: $NODE_DIFF"
+        TS=$(date)
+        NODE_DIFF=$($TON_CONSOLE -C $TON_CONSOLE_CONFIG -c getstats | grep 'timediff' | sed 's/[[:space:]]*"timediff":[[:space:]]*//g' | sed 's/,//g')
+        if [ -z $NODE_DIFF ]; then
+           NODE_DIFF="-1"
+        fi
 }
 
 if [ $cycle_view -eq 0 ]; then
-	show_node_diff
+        show_node_diff
+        echo $NODE_DIFF
 fi
 
 while [ $cycle_view -gt 0 ]; do
-	show_node_diff; sleep $cycle_interval
+        show_node_diff;
+        echo "${TON_DAPP} ${TS}: $NODE_DIFF"
+        sleep $cycle_interval
 done
+
