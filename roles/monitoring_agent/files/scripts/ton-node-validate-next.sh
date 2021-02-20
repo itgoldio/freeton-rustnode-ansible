aa165893b29fe3c4fede4ac0e58b9fd91e9af26e
Gavrilov, Evgueni <evgueni.gavrilov@vStack.com>
2021-02-20 23:52:38 +0300

    there's no need to exec sed twice

--- roles/monitoring_agent/files/scripts/ton-node-validate-next.sh
+++ roles/monitoring_agent/files/scripts/ton-node-validate-next.sh
@@ -7,7 +7,7 @@ ton-check-env.sh TON_CONSOLE
 ton-check-env.sh TON_CONSOLE_CONFIG
 
 
-IN_NEXT_ROUND=$($TON_CONSOLE -C $TON_CONSOLE_CONFIG -c getstats | grep 'in_next_vset_p36' | sed 's/[[:space:]]*"in_next_vset_p36":[[:space:]]*//g' | sed 's/,//g')
+IN_NEXT_ROUND=$($TON_CONSOLE -C $TON_CONSOLE_CONFIG -c getstats | grep 'in_next_vset_p36' | sed 's/[[:space:]]*"in_next_vset_p36":[[:space:]]*//g;s/,//g')
 if [ -z $IN_NEXT_ROUND ]; then
    echo "UNKNOWN"
    exit
